1eb493392eb1e4e437ec62ca87b7bc6a4f4d729e
Bima Derian <derian7680@gmail.com>
2023-03-05 00:32:00 +0700

    .

--- segitiga_for.py
+++ segitiga_for.py
@@ -1,7 +1,18 @@
+"""
+{
+    Author : Bima Derian Al-Bachry,
+    Kelas : 02TPLP004,
+},{
+    Semester : 002,
+    Fakultas : Ilmu Komputer,
+    Jurusan : Teknik Informatika,
+}
+"""
+
 # Membuat segitiga dengan for loop
 def satu():
     alas = int(input('Masukan Alas :'))
-    for i in range(0, a):
+    for i in range(0, alas):
         for j in range(0, i + 1):
             print('* ' , end='')
         print('')
@@ -9,20 +20,20 @@ def satu():
 
 def dua():
     alas = int(input('Masukan Alas :'))
-    for i in range(0, a):
-        for j in range(0, a - 1):
+    for i in range(0, alas):
+        for j in range(0, alas - 1):
             print('* ' , end='')
-        a -= 1
+        alas -= 1
         print('')
     
 
 def tiga():
     alas = int(input('Masukan Alas :'))
-    s = 2 * a - 2 # for spaces
-    for i in range(0, a):
-        for j in range(0, s):
+    sisi = 2 * alas - 2 # for spaces
+    for i in range(0, alas):
+        for j in range(0, sisi):
             print(' ',end='')
-        s -= 2
+        sisi -= 2
         for j in range(0, i + 1):
             print('* ', end='')
         print('')
@@ -30,25 +41,25 @@ def tiga():
 
 def empat():
     alas = int(input('Masukan Alas :'))
-    s = 0 # for spaces
-    for i in range(0, a):
-        for j in range(0, s):
+    sisi = 0 # for spaces
+    for i in range(0, alas):
+        for j in range(0, sisi):
             # print(j, end='')
             print(' ',end='')
-        s += 2
-        for j in range(0, a):
+        sisi += 2
+        for j in range(0, alas):
             print('* ' , end='')
-        a -= 1
+        alas -= 1
         print('')
     
 
 def lima():
     alas = int(input('Masukan Alas :'))
-    s = a - 1 # for spaces
-    for i in range(0, a):
-        for j in range(0, s):
+    sisi = alas - 1 # for spaces
+    for i in range(0, alas):
+        for j in range(0, sisi):
             print(' ', end='')
-        s -= 1
+        sisi -= 1
         for j in range(0, i + 1):
             print('* ', end='')
     
@@ -57,12 +68,20 @@ def lima():
 
 def enam():
     alas = int(input('Masukan Alas :'))
-    s = 0 # for spaces
-    for i in range(0, a):
-        for j in range(0, s):
+    sisi = 0 # for spaces
+    for i in range(0, alas):
+        for j in range(0, sisi):
             print(' ',end='')
-        s += 1
-        for j in range(0, a):
+        sisi += 1
+        for j in range(0, alas):
             print('* ' , end='')
-        a -= 1
+        alas -= 1
         print('')
+      
+# execute program
+satu()
+dua()
+tiga()
+empat()
+lima()
+enam()
